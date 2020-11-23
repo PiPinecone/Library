df6b7cb721f470e6b8ecae4c9eb2c0e84486634d
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-11-23 10:22:48 -0700

    yaml stuff

--- input.py
+++ input.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient, ReturnDocument
 import yaml
 
 
-with open("config.yaml", 'r') as stream:
+with open("cred.yaml", 'r') as stream:
     data = yaml.safe_load(stream)
     USER = data["username"]
     PASS = data["password"]
